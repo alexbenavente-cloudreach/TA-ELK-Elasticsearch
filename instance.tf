80be9dd2eeaf741c4f1ec94ca35b446957edac5e
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-04-27 13:55:45 -0400

    Testing GitHub Actions

--- instance.tf
+++ instance.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "elasticsearch_server" {
-  count                  = 1
-  ami                    = data.aws_ami.aws_ubuntu_server.id
-  instance_type          = var.ec2_type
-  subnet_id              = data.aws_subnet.private.id
-  vpc_security_group_ids = [aws_security_group.elasticsearch_sg.id]
-  key_name               = var.my_keypair
+  count                       = 1
+  ami                         = data.aws_ami.aws_ubuntu_server.id
+  instance_type               = var.ec2_type
+  subnet_id                   = data.aws_subnet.private.id
+  vpc_security_group_ids      = [aws_security_group.elasticsearch_sg.id]
+  key_name                    = var.my_keypair
   associate_public_ip_address = true
-  
+
 
   tags = {
     Name = "elk1_elasticsearch_server"
--- security-groups.tf
+++ security-groups.tf
@@ -9,23 +9,23 @@ resource "aws_security_group" "elasticsearch_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["192.168.0.0/16"] 
+    cidr_blocks = ["192.168.0.0/16"]
   }
   ingress {
     description = "ingress rules"
-    from_port = 9200
-    to_port = 9300
-    protocol = "tcp"
+    from_port   = 9200
+    to_port     = 9300
+    protocol    = "tcp"
     cidr_blocks = ["192.168.0.0/16"]
   }
   egress {
     description = "egress rules"
-    cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
   }
-  tags={
-    Name="elasticsearch_sg"
+  tags = {
+    Name = "elasticsearch_sg"
   }
 }
